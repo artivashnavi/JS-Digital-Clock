a1880a7069bcde63428f6234a9915d53aebb464f
Arti Rathaur <111077845+artivashnavi@users.noreply.github.com>
2022-12-23 23:45:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,63 +3,29 @@ let hrs = 0;
 let mins = 0;
 let secs = 0
 
-
-
-
 function clockTime() {
-   const date = new Date();
-    // console.log(date);
+   const date = new Date();//console.log(date);
 
-   let hours = date.getHours();
-  //  console.log(hours);
-  hrs = hours;
+   let hours = date.getHours();//console.log(hours);
+    hrs = hours;
 
-   let min = date.getMinutes();
-  //  console.log(min);
-mins = min;
-   let sec = date.getSeconds();
-  //  console.log(sec);
-secs = sec;
+    let min = date.getMinutes();//console.log(min);
+    mins = min;
 
-   if (hours >= 20 && min >= 0 && sec >= 1) {
-       document.getElementById("imgM").src = "./sleep.png";
-       document.getElementById('text').innerHTML = "GOOD NIGHT!!"
-       document.getElementById('grabText').innerHTML = "CLOSE YOUR EYES AND GO TO SLEEP"
-      
-   }
-   else if (hours >= 16 && min >= 0 && sec >= 1) {
-       document.getElementById("imgM").src = "./evening.png";
-       document.getElementById('text').innerHTML = "GOOD EVENING!!"
-       document.getElementById('grabText').innerHTML = "STOP YAMING, GET SOME TEA ITS JUST EVENING"
-   }
-   else if (hours >= 12 && min >= 0 && sec >= 1) {
-       document.getElementById("imgM").src = "./afternoon.png";
-       document.getElementById('text').innerHTML = "GOOD AFTERNOON!! TAKE SOME SLEEP!!"
-       document.getElementById('grabText').innerHTML = "LET'S HAVE SOME LUNCH !!"
-   }
-   else if (hours >= 10 && min >= 0 && sec >= 1) {
-       document.getElementById("imgM").src = "./girl.png";
-       document.getElementById('text').innerHTML = "GOOD MORNING!! WAKE UP !!"
-       document.getElementById('grabText').innerHTML = "GRAB SOME HEALTHY BREAKFAST"
-   }
+    let sec = date.getSeconds();//console.log(sec);
+    secs = sec;
 
-
-   let meridian;
+  let meridian;
    if (hours >= 12) {
        meridian = "PM"
        hours -= 12;
-   }
-   else {
+   }else {
        meridian = "AM"
    }
-// if (hours < 10)
-   //     hours = "0" + hours;
+
    hours = hours < 10 ? "0" + hours : hours;
-
-   min = min < 10 ?  "0" + min : min;
-   
+   min = min < 10 ?  "0" + min : min; 
    sec = sec < 10 ? sec = "0" + sec : sec;
-
 
    const hoursSlot = document.getElementById("hours")
    hoursSlot.innerHTML = `${hours}` + "\n hours";
@@ -80,11 +46,7 @@ setInterval(() => {
 }, 1000);
 
 function setAlarmTimer() {
-   // alarm_detail
-   // wake_up
-   // lunch_time
-   // nap_time
-   // night_time
+   // alarm_detail// wake_up// lunch_time// nap_time// night_time
    let first = document.getElementById("morningtime").value;
    console.log(first,hrs);
    if(first == hrs){
@@ -113,27 +75,15 @@ function setAlarmTimer() {
    console.log(four,hrs);
    if(four == hrs){
     document.getElementById("imgM").src = "./sleep.png";
-       document.getElementById('text').innerHTML = "GOOD NIGHT!!"
-       document.getElementById('grabText').innerHTML = "CLOSE YOUR EYES AND GO TO SLEEP"
-      
-   }
+    document.getElementById('text').innerHTML = "GOOD NIGHT!!"
+    document.getElementById('grabText').innerHTML = "CLOSE YOUR EYES AND GO TO SLEEP"
+  }
 
-
-
-
-
-
-   document.getElementsByClassName("addtext")[0].innerHTML =
-     " Wake up time :" + document.getElementById("morningtime").value;
-
-   document.getElementsByClassName("addtext")[1].innerHTML =
-     " Lunch time :" + document.getElementById("lunchtime").value;
-
-   document.getElementsByClassName("addtext")[2].innerHTML =
-     " Nap time :" + document.getElementById("naptime").value;
-
-   document.getElementsByClassName("addtext")[3].innerHTML =
-     " Night time :" + document.getElementById("nighttime").value;
+  let update = document.getElementsByClassName("addtext");
+  update[0].innerText =  " Wake up time :" + " " + morningtime.options[morningtime.selectedIndex].text;
+  update[1].innerText =  " Lunch time :" + " " + lunchtime.options[lunchtime.selectedIndex].text;
+  update[2].innerText =  " Nap time :" + " " + naptime.options[naptime.selectedIndex].text;
+  update[3].innerText =  " Night time :" + " " + nighttime.options[nighttime.selectedIndex].text;
 
     
  }
